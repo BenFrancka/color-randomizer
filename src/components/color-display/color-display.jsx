36cf017ce2a470c5aa1449d8d68ddca43b63df1e
BenFrancka <benfrancka@gmail.com>
2021-09-07 16:43:49 -0700

    adds base styling to color-display component

--- src/components/color-display/color-display.jsx
+++ src/components/color-display/color-display.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './color-display.css';
 
 const Display = ({ color }) => {
   return <div style={{ backgroundColor: color }}></div>;
