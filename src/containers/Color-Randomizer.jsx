918a0dc282428f6718a3e18ef3683eed98ea9149
BenFrancka <benfrancka@gmail.com>
2021-09-07 16:20:57 -0700

    adds color randomizer function to Container

--- src/containers/Color-Randomizer.jsx
+++ src/containers/Color-Randomizer.jsx
@@ -6,6 +6,16 @@ class ColorRandomizer extends Component {
       color: '#000000'
     };
 
+    const setColor = () => {
+        const randomizeColor = 
+        Math.floor(Math.random()*16777215).toString(16);
+        const randomColorString = '#' + randomizeColor;
+    }
+
+    
+
+
+
     render() {
       return (
         <>
